d26c35b09acf57072cef4f2ffde0ea48160fd80c
Bakai <bahaerkinbaev8@gmail.com>
2024-05-04 19:43:24 +0200

    add migration example

---
+++ migration/main.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+
+engine = create_engine("postgresql+psycopg2://myuser:mypassword@0.0.0.0:5432/mydb")
+
+
+def app():
+    with engine.connect() as conn:
+        stmt = text("select * from pg_database")
+        print(conn.execute(stmt).fetchall())
+
+
+if __name__ == "__main__":
+    app()
